744a181739d2462939ec5118a54b4c01cccd7c13
YouFiveT <dbxor1568@naver.com>
2021-10-27 21:06:07 +0900

    웹팩 완벽

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,26 @@
-const nodeExternals = require("webpack-node-externals");
+const webpack = require("webpack");
+const path = require("path");
 
 module.exports = {
-  target: "node",
-  externals: [nodeExternals()],
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist/",
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname),
+        exclude: /(node_modules)|(dist)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["env"],
+          },
+        },
+      },
+    ],
+  },
 };
